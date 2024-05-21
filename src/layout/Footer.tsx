60f82951c9720407b3261956590efa4029d6e634
Movin Silva <movinpinsara@gmail.com>
2024-05-21 10:54:55 +0530

    feat: made it to only the app stage

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,25 @@
+import './footer.scss';
+
+const Footer = () => (
+  <div className="sign-in-footer">
+    <div className="footer">
+      By continuing, you agree to Interest's
+      <br />
+      <a href="#">Terms of Service</a> and acknowledge you've read our
+      <br />
+      <a href="#">Privacy Policy. </a>
+      <a href="#s">Notice at collection.</a>
+    </div>
+
+    <div className="divider" />
+
+    <div className="footer footer-bottom">
+      <a href="#">Not on Interest yet? Sign up </a>
+      <br />
+      <span className="business-text"> Are you a business? </span>
+      <a href="#">Get started here!</a>
+    </div>
+  </div>
+);
+
+export default Footer;
--- src/layout/SignInButton.tsx
+++ src/layout/SignInButton.tsx
@@ -1,12 +1,4 @@
-import React, { ReactElement, useState } from "react";
-import {
-  Hooks,
-  SignIn,
-  useOn,
-  SignedOut,
-  SignOutButton,
-  SignedIn
-} from "../../../../web-ui-sdks/packages/react/src";
+import { ReactElement, useState } from "react";
 import "./sign-in-button.scss";
 
 const SignInButton = (): ReactElement => {
@@ -20,20 +12,6 @@ const SignInButton = (): ReactElement => {
     setModalVisible(false);
   };
 
-  const func = () => {
-    console.log("func works new");
-  }
-
-  useOn({
-    event: Hooks.SignIn,
-    callback: func,
-  });
-
-  useOn({
-    event: Hooks.SignOut,
-    callback: () => {window.location.reload()},
-  });
-
   return (
     <div className="asgardeo">
       <button className="navbar__button login-btn" onClick={openModal}>
@@ -45,43 +23,7 @@ const SignInButton = (): ReactElement => {
           <button className="close-button" onClick={closeModal}>
             X
           </button>
-          <SignIn
-            showFooter={false}
-            brandingProps={{
-              preference: {
-                text: {
-                  "en-US": {
-                    common: {
-                      "multiple.options.prefix": "Continue with",
-                    },
-                    login: {
-                      username: "Email",
-                      "enter.your.username": "Email",
-                      "enter.your.password": "Password",
-                    },
-                  },
-                },
-              },
-            }}
-          />
-          <div className="footer">
-            By continuing, you agree to Interest's
-            <br />
-            <a href="#">Terms of Service</a> and acknowledge you've read our
-            <br />
-            <a href="#">Privacy Policy. </a>
-            <a href="#s">Notice at collection.</a>
-          </div>
-
-          <div className="divider" />
-
-          <div className="footer footer-bottom">
-            <a href="#">Not on Interest yet? Sign up </a>
-            <br />
-            <span className="business-text"> Are you a business? </span>
-            <a href="#">Get started here!</a>
-          </div>
-          <SignedIn><SignOutButton /></SignedIn>
+          
         </div>
       )}
 
